8f3dc1ccd5d7d9061b82d78ba8f549a0ebf46b20
Achild0 <lesbatsvirgile@hotmail.com>
2021-04-19 15:30:12 +0200

    Instensiation player dans main

--- Dodge The creeps!/Main.gd
+++ Dodge The creeps!/Main.gd
@@ -1,14 +1,12 @@
 extends Node
 
-
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	$Player.spawn($Position2D.position)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
